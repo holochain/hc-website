3c0ec6c7517c2b2cacc1c9dc1524012373bbdfac
Kharleann Cabrera <kharleann.cabrera@eyss.io>
2021-02-18 11:32:38 -0400

    added css

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const CleanCSS = require("clean-css");
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addFilter("cssmin", function (code) {
+		return new CleanCSS({}).minify(code).styles;
+	});
 	eleventyConfig.addPassthroughCopy("images");
 	eleventyConfig.addPassthroughCopy("admin");
 };
